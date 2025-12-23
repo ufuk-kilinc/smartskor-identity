5c70b4d785bd5bb2db2e14872fc714c7d7ade9af
KAAN-LAPTOP\kaanu <kaan@kreaydalabs.com>
2025-12-23 12:24:42 +0300

    Refresh token fixes...

--- Extensions/OpenIddictExtensions.cs
+++ Extensions/OpenIddictExtensions.cs
@@ -18,7 +18,8 @@ public static class OpenIddictExtensions
                 options.SetAuthorizationEndpointUris("/connect/authorize")
                     .SetTokenEndpointUris("/connect/token")
                     .SetUserInfoEndpointUris("/connect/userinfo")
-                    .SetEndSessionEndpointUris("/connect/logout");
+                    .SetEndSessionEndpointUris("/connect/logout")
+                    .SetIntrospectionEndpointUris("/connect/introspect");
 
                 // Enable authorization code flow with PKCE
                 options.AllowAuthorizationCodeFlow()
@@ -27,8 +28,16 @@ public static class OpenIddictExtensions
                 // Enable refresh tokens
                 options.AllowRefreshTokenFlow();
 
+                // Disable refresh token rotation (prevents "already redeemed" errors with concurrent requests)
+                options.DisableRollingRefreshTokens();
+
+                // Set token lifetimes
+                options.SetAccessTokenLifetime(TimeSpan.FromHours(1));
+                options.SetRefreshTokenLifetime(TimeSpan.FromDays(14));
+                options.SetIdentityTokenLifetime(TimeSpan.FromMinutes(30));
+
                 // Register scopes
-                options.RegisterScopes("openid", "profile", "email", "offline_access", "api");
+                options.RegisterScopes("openid", "profile", "email", "offline_access", "smartskor-api");
 
                 // Development certificates (replace in production)
                 options.AddDevelopmentEncryptionCertificate()
